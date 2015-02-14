41739eef0710dd93753f4345e176489f51608fff
John Fox <john@classicyuppie.com>
2015-02-14 16:43:15 -0500

    index.js to include ghostHunter parameters
    Modified index.js to include ghostHunter parameters

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,12 @@
         // for better relative/absolute positioning.
 
         //$("#blog-logo").prependTo("#site-head-content");
-
+		$("#search").ghostHunter({
+              results: "#search-results",
+              zeroResultsInfo : false,
+              info_template   : "<p>Number of posts found: {{amount}}</p>",
+              onKeyUp         : true
+        });  
     });
 
 }(jQuery));
